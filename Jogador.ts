33fe4c3aea0ae7c5c4444837aa17cefbcc31cfda
Eduardo <eduardobragadeoliveira@gmail.com>
2024-07-29 23:50:29 -0400

    Praticamente finalizado, falta tirar duvida com o professor sobre o UML e sobre o retorno da classe principal

--- Jogador.ts
+++ Jogador.ts
@@ -1,69 +1,82 @@
+import { Campeao } from "./Campeao";
+import { Time } from "./Time";
 
 export class Jogador{
-    private _nick : string
-    private _elo:number
-    private _nivel:number
+    private _nick: string
+    private _elo: string
+    private _nivel: number
     private _dataRegistro: Date
-    private _times: Times[]
+    private _times: Time[]
     private _campeoes: Campeao[]
-     
-     constructor(nick:string, elo:number, nivel:number, dataRegistro:Date, times:Times[], campeoes:Campeoes[]){  
+
+    constructor(nick: string, elo: string, nivel: number, dataRegistro: Date, times: Time[], campeoes: Campeao[]) {
         this._nick = nick;
         this._elo = elo;
         this._nivel = nivel;
         this._dataRegistro = dataRegistro
-        this._times = times 
+        this._times = times
         this._campeoes = campeoes
-
     }
-get nick(){
-return this.nick;
-}
-set nick(nick: string){
-   this.nick = nick;
-}
-get elo(){
-    return this.elo;
-}
-set elo(elo:number){
-     this.elo=elo;
-}
-get nivel(){
-    return this.nivel;
-}
-set nivel(nivel:number){
-     this._nivel=nivel;
-}
-get dataRegistro(){
-    return this._dataRegistro;
-}
-set dataRegistro(elo:number){
-     this._dataRegistro = dataRegistro;
-}
-get times(){
-    return this._times.push;
-}
-adicionartime(times: Times){
-    this._times.push(times);
-}
 
-removertime(times: Times){
-    let index = this._times.indexOf(times);
-    if(index >= 0){
-        this._times.splice(index, 1);
-    }        
-}
-get campeoes(){
-    return this._campeoes.push;
-}
-adicionarcampeoes(campeoes: Campeoes){
-    this._campeoes.push(campeoes);
-}
+    get nick(): string {
+        return this.nick;
+    }
 
-removercampeoes(campeoes: Campeoes){
-    let index = this._campeoes.indexOf(campeoes);
-    if(index >= 0){
-        this._campeoes.splice(index, 1);
-    }        
-}
+    set nick(nick: string) {
+        this._nick = nick;
+    }
+
+    get elo(): string {
+        return this._elo;
+    }
+
+    set elo(elo: string) {
+        this._elo = elo;
+    }
+    
+    get nivel(): number {
+        return this.nivel;
+    }
+    
+    set nivel(nivel: number) {
+        this._nivel = nivel;
+    }
+
+    get dataRegistro(): Date {
+        return this._dataRegistro;
+    }
+
+    set dataRegistro(dataRegistro: Date) {
+        this._dataRegistro = dataRegistro;
+    }
+
+    get times(): Time[] {
+        return this._times;
+    }
+
+    adicionarTime(time: Time) {
+        this._times.push(time);
+    }
+
+    removerTime(time: Time) {
+        let index = this._times.indexOf(time);
+        if (index >= 0) {
+            this._times.splice(index, 1);
+        }
+    }
+
+    get campeoes(): Campeao[] {
+        return this._campeoes;
+    }
+
+    adicionarCampeoes(campeao: Campeao) {
+        this._campeoes.push(campeao);
+    }
+
+    removerCampeoes(campeao: Campeao) {
+        let index = this._campeoes.indexOf(campeao);
+        if (index >= 0) {
+            this._campeoes.splice(index, 1);
+        }
+    }
 }
---
+++ programa.ts
@@ -0,0 +1,18 @@
+import { Campeao } from "./Campeao";
+import { Time } from "./Time";
+import { Jogador } from "./jogador";
+
+let rell = new Campeao('Rell', 'Jungle', 150);
+let zyra = new Campeao('Zyra', 'Jungle', 170);
+let tryndamere = new Campeao('Tryndamere', 'Top', 55);
+
+let cariok = new Jogador('Cariok', 'Desafiante', 500, new Date("2014-01-01"), [], [rell, zyra]);
+
+let pain  = new Time('Pain Gaming', 4, 1, [cariok]);
+
+let xkflayz = new Jogador('xkflayz', 'Prata', 380, new Date("2020-01-01"), [pain], [tryndamere]);
+
+console.log(xkflayz);
+console.log(xkflayz.times);
+console.log(pain);
+console.log(cariok);
