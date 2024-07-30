13eaeed517f5b4711a5efab6d3e2bfdcad65e770
Matheus Pardi Almeida <matheuspardialmeida@hotmail.com>
2024-07-29 23:02:53 -0400

    Time quase terminado

---
+++ Time.ts
@@ -0,0 +1,42 @@
+export class Time{
+    private _nome:string
+    private _qtdTitulos:number
+    private _tier:number
+    private _jogadores:Jogador[]
+
+    constructor(nome:string, qtdTitulos:number, tier:number, jogadores:Jogador[]){
+        this._nome = nome
+        this._qtdTitulos = qtdTitulos
+        this._tier = tier
+        this._jogadores = jogadores
+    }
+
+    get nome(){
+        return this._nome;
+    }
+    set nome(nome:string){
+        this._nome = nome;
+    }
+
+    get qtdTitulos(){
+        return this._qtdTitulos;
+    }
+    set qtdTitulos(qtdTitulos:number){
+        this._qtdTitulos = qtdTitulos;
+    }
+
+    get tier(){
+        return this._tier;
+    }
+    set tier(tier:number){
+        this._tier = tier;
+    }
+
+    get jogadores(){
+        return this._jogadores;
+    }
+    set jogadores(jogadores:Jogador[]){
+        this._jogadores = jogadores;
+    }
+    
+}
