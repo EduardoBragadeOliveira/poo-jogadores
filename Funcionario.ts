efab02f20632157d2d59eebecbf33ac5e8d42943
Matheus Pardi Almeida <matheuspardialmeida@hotmail.com>
2024-07-31 21:42:02 -0400

    Funcionario terminado

---
+++ Funcionario.ts
@@ -0,0 +1,26 @@
+export class Funcionario{
+    private _nome:string
+    private _cargo:string
+    private _salario:number
+
+    constructor(nome:string, cargo:string, salario:number){
+        this._nome = nome
+        this._cargo = cargo
+        this._salario = salario
+    }
+
+    get cargo(){
+        return this._cargo;
+    }	
+    set cargo(cargo:string){
+        this._cargo = cargo;
+    }
+
+    get salario(){
+        return this._salario;
+    }
+    set salario(salario:number){
+        this._salario = salario;
+    }
+
+}
